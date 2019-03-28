d5603ec93aca7cbca1d08cdacc2ec557af491ecb
Maxim-Evmenkov <max.y@viber.com>
2019-03-28 23:17:45 +0300

    GridItemDecoration refactored

--- app/src/main/java/com/school/kt/imagefilters/ui/GridItemDecoration.kt
+++ app/src/main/java/com/school/kt/imagefilters/ui/GridItemDecoration.kt
@@ -7,46 +7,47 @@ import android.view.View
 /***
  * Made by Lokesh Desai (Android4Dev)
  */
-class GridItemDecoration(gridSpacingPx: Int, gridSize: Int) : RecyclerView.ItemDecoration() {
-    private var mSizeGridSpacingPx: Int = gridSpacingPx
-    private var mGridSize: Int = gridSize
+class GridItemDecoration(private val gridSpacingPx: Int, private val gridSize: Int) : RecyclerView.ItemDecoration() {
 
-    private var mNeedLeftSpacing = false
+    private var needLeftSpacing = false
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        val frameWidth = ((parent.width - mSizeGridSpacingPx.toFloat() * (mGridSize - 1)) / mGridSize).toInt()
-        val padding = parent.width / mGridSize - frameWidth
-        val itemPosition = (view.getLayoutParams() as RecyclerView.LayoutParams).viewAdapterPosition
-        if (itemPosition < mGridSize) {
-            outRect.top = 0
-        } else {
-            outRect.top = mSizeGridSpacingPx
-        }
-        if (itemPosition % mGridSize == 0) {
-            outRect.left = 0
-            outRect.right = padding
-            mNeedLeftSpacing = true
-        } else if ((itemPosition + 1) % mGridSize == 0) {
-            mNeedLeftSpacing = false
-            outRect.right = 0
-            outRect.left = padding
-        } else if (mNeedLeftSpacing) {
-            mNeedLeftSpacing = false
-            outRect.left = mSizeGridSpacingPx - padding
-            if ((itemPosition + 2) % mGridSize == 0) {
-                outRect.right = mSizeGridSpacingPx - padding
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) =
+        with(outRect) {
+            val frameWidth = ((parent.width - gridSpacingPx.toFloat() * (gridSize - 1)) / gridSize).toInt()
+            val padding = parent.width / gridSize - frameWidth
+            val itemPosition = (view.layoutParams as RecyclerView.LayoutParams).viewAdapterPosition
+
+            top = if (itemPosition < gridSize) {
+                0
             } else {
-                outRect.right = mSizeGridSpacingPx / 2
+                gridSpacingPx
             }
-        } else if ((itemPosition + 2) % mGridSize == 0) {
-            mNeedLeftSpacing = false
-            outRect.left = mSizeGridSpacingPx / 2
-            outRect.right = mSizeGridSpacingPx - padding
-        } else {
-            mNeedLeftSpacing = false
-            outRect.left = mSizeGridSpacingPx / 2
-            outRect.right = mSizeGridSpacingPx / 2
+
+            if (itemPosition % gridSize == 0) {
+                left = 0
+                right = padding
+                needLeftSpacing = true
+            } else if ((itemPosition + 1) % gridSize == 0) {
+                needLeftSpacing = false
+                right = 0
+                left = padding
+            } else if (needLeftSpacing) {
+                needLeftSpacing = false
+                left = gridSpacingPx - padding
+                if ((itemPosition + 2) % gridSize == 0) {
+                    outRect.right = gridSpacingPx - padding
+                } else {
+                    outRect.right = gridSpacingPx / 2
+                }
+            } else if ((itemPosition + 2) % gridSize == 0) {
+                needLeftSpacing = false
+                left = gridSpacingPx / 2
+                right = gridSpacingPx - padding
+            } else {
+                needLeftSpacing = false
+                left = gridSpacingPx / 2
+                right = gridSpacingPx / 2
+            }
+            bottom = 0
         }
-        outRect.bottom = 0
-    }
 }
