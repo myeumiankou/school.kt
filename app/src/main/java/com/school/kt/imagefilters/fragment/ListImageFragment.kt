8414b40bc18e89fee08502c333cc6c993b7c1f2d
Maxim-Evmenkov <max.y@viber.com>
2019-03-28 22:21:23 +0300

    refactored ListImageFragment:
    1) made use of Kotlin extensions, which allow to skip "findViewById" procedure
    2) made use of "with" Kotlin extension function
    3) removed redundant fields declaration

--- app/src/main/java/com/school/kt/imagefilters/fragment/ListImageFragment.kt
+++ app/src/main/java/com/school/kt/imagefilters/fragment/ListImageFragment.kt
@@ -3,10 +3,8 @@ package com.school.kt.imagefilters.fragment
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.*
 import android.widget.SearchView
-import android.widget.TextView
 import android.widget.Toast
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -18,19 +16,19 @@ import com.school.kt.imagefilters.repository.ListImageRepository
 import com.school.kt.imagefilters.ui.GridItemDecoration
 import com.school.kt.imagefilters.ui.ImageAdapter
 import com.school.kt.imagefilters.view.ListImageView
+import kotlinx.android.synthetic.main.list_image_fragment_layout.*
 import org.koin.android.ext.android.inject
-
 
 class ListImageFragment : MvpAppCompatFragment(), ListImageView, SearchView.OnQueryTextListener {
 
-    private val repository : ListImageRepository by inject()
-    private val uiHandler : Handler by inject()
+    private val repository: ListImageRepository by inject()
+    private val uiHandler: Handler by inject()
 
     @InjectPresenter
     lateinit var presenter: ListImagePresenter
 
     @ProvidePresenter
-    fun providePresenter() : ListImagePresenter {
+    fun providePresenter(): ListImagePresenter {
         return ListImagePresenter(repository, uiHandler)
     }
 
@@ -38,57 +36,59 @@ class ListImageFragment : MvpAppCompatFragment(), ListImageView, SearchView.OnQu
         const val IMAGE_ROW_COUNT = 4
     }
 
-    private var messageView: TextView? = null
-    private var recyclerView: RecyclerView? = null
-    private var adapter = ImageAdapter()
-    private var searchView: SearchView? = null
+    private lateinit var searchView: SearchView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.list_image_fragment_layout, container, true)
-        messageView = view.findViewById(R.id.messageView)
-        recyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView?.adapter = adapter
-        recyclerView?.layoutManager = GridLayoutManager(context, IMAGE_ROW_COUNT)
-        recyclerView?.addItemDecoration(GridItemDecoration(10, IMAGE_ROW_COUNT))
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.list_image_fragment_layout, container, true)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(recyclerView) {
+            adapter = ImageAdapter()
+            layoutManager = GridLayoutManager(context, IMAGE_ROW_COUNT)
+            addItemDecoration(GridItemDecoration(10, IMAGE_ROW_COUNT))
+        }
         setHasOptionsMenu(true)
-        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.search, menu)
-        val menuItem = menu?.findItem(R.id.search)
-        searchView = menuItem!!.actionView as SearchView
-        searchView!!.setOnQueryTextListener(this)
+        inflater!!.inflate(R.menu.search, menu)
+        if (!::searchView.isInitialized) {
+            searchView = menu?.findItem(R.id.search)!!.actionView as SearchView
+        }
+        searchView.setOnQueryTextListener(this)
     }
 
-    override fun onQueryTextChange(query: String): Boolean {
-        return true
-    }
+    override fun onQueryTextChange(query: String) = true
 
     override fun onQueryTextSubmit(query: String?): Boolean {
         presenter.searchImages(query)
         return true
     }
 
-    override fun showListImages(images: List<Image>) {
-        adapter.images = images
-        adapter.notifyDataSetChanged()
+    override fun showListImages(images: List<Image>) = with(recyclerView.adapter as ImageAdapter) {
+        this.images = images
+        notifyDataSetChanged()
     }
 
     override fun showProgress() {
-        adapter.images = emptyList()
-        adapter.notifyDataSetChanged()
-        messageView?.text = "Loading..."
+        with(recyclerView.adapter as ImageAdapter) {
+            images = emptyList()
+            notifyDataSetChanged()
+        }
+        messageView.text = "Loading..."
     }
 
     override fun hideProgress() {
-        messageView?.text = ""
+        messageView.text = ""
     }
 
     override fun showNoResultView() {
-        adapter.images = emptyList()
-        adapter.notifyDataSetChanged()
-        messageView?.text = "No results. Try another search!"
+        with(recyclerView.adapter as ImageAdapter) {
+            images = emptyList()
+            notifyDataSetChanged()
+        }
+
+        messageView.text = "No results. Try another search!"
     }
 
     override fun showResultCount(count: Int) {
@@ -96,10 +96,10 @@ class ListImageFragment : MvpAppCompatFragment(), ListImageView, SearchView.OnQu
     }
 
     override fun showFail(error: Int) {
-        messageView?.text = "Fail. Code = $error"
+        messageView.text = "Fail. Code = $error"
     }
 
     override fun showError(error: String?) {
-        messageView?.text = "Error. Message: $error"
+        messageView.text = "Error. Message: $error"
     }
 }
