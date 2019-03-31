f03378e0b34d0d19a78704e40b1241b2d74d6b3e
Maxim-Evmenkov <max.y@viber.com>
2019-03-31 16:00:25 +0300

    Image as Parcelable

--- app/src/main/java/com/school/kt/imagefilters/data/Image.kt
+++ app/src/main/java/com/school/kt/imagefilters/data/Image.kt
@@ -1,14 +1,17 @@
 package com.school.kt.imagefilters.data
 
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 class Image(
     @Expose
     val url: String,
     @SerializedName("large_url")
     @Expose
     val largeUrl: String
-) {
+) : Parcelable {
     override fun toString() = "url - $url - large url $largeUrl"
 }
