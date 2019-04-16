91355c1cebf98839b917b5cd99cbda856c2dd605
Maxim-Evmenkov <max.y@viber.com>
2019-04-16 22:17:44 +0300

    Getting image from glide using Future

--- app/src/main/java/com/school/kt/imagefilters/presenter/EditImagePresenter.kt
+++ app/src/main/java/com/school/kt/imagefilters/presenter/EditImagePresenter.kt
@@ -1,6 +1,7 @@
 package com.school.kt.imagefilters.presenter
 
 import android.Manifest
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Handler
 import android.support.annotation.RequiresPermission
@@ -9,18 +10,20 @@ import com.arellomobile.mvp.InjectViewState
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.FutureTarget
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.school.kt.imagefilters.data.Image
 import com.school.kt.imagefilters.di.FileFolder
 import com.school.kt.imagefilters.di.GlideOptions.bitmapTransform
-import com.school.kt.imagefilters.ui.FileTarget
 import com.school.kt.imagefilters.view.EditImageView
 import jp.wasabeef.glide.transformations.BitmapTransformation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 @InjectViewState
@@ -48,7 +51,7 @@ class EditImagePresenter(
             val directory = File(externalFileDir, File.separator + FileFolder.SAVED_IMAGE_FOLDER_NAME + File.separator)
             directory.mkdirs()
             val outFile = createTempFile("IMG", ".jpg", directory)
-            obtainImageFile(outFile.absolutePath, currentTransformation)
+            saveImage(outFile.path, obtainImageFile(currentTransformation).get())
             viewState.notifyGalleryAboutNewImage(outFile)
 
             outFile
@@ -56,21 +59,42 @@ class EditImagePresenter(
 
         Log.e("EditImagePresenter", "notifying UI in " + Thread.currentThread())
         viewState.showToastMessage("saved to $result")
+        viewState.showMessage("saved!")
     }
 
     fun shareImage() = ioScope.launch {
         with(createTempFile(suffix = ".jpg")) {
-            obtainImageFile(path, currentTransformation)
+            saveImage(path, obtainImageFile(currentTransformation).get())
             viewState.shareImage(this)
+
+            withContext(Dispatchers.Main) {
+                viewState.showMessage("shared!")
+            }
         }
     }
 
-    private fun obtainImageFile(fileName: String, currentTransformation: BitmapTransformation?) {
+    private fun obtainImageFile(currentTransformation: BitmapTransformation?): FutureTarget<Bitmap> =
         with(glide.asBitmap().load(image.url)) {
             currentTransformation?.let {
                 apply(bitmapTransform(currentTransformation))
             }
-            into(FileTarget(fileName))
+            submit()
+        }
+
+    private fun saveImage(
+        fileName: String,
+        bitmap: Bitmap,
+        quality: Int = 70,
+        format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG
+    ) {
+        try {
+            with(FileOutputStream(fileName)) {
+                bitmap.compress(format, quality, this)
+                flush()
+                close()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
